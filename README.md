f05354bb9e1e1b3ea3f7027fbfa81209a493a15c
Josh Lapham <josh@joshlapham.com>
2013-03-22 14:30:56 +1100

    Changed function to accept URL as parameter; added more comments; misc formatting changes to all files; added TODO section to README; removed TODO file.

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
-## instacli
-
-A simple Python script that takes a URL as an argument and adds to Instapaper.
+## InstaCLI
 
 By Josh Lapham [josh@joshlapham.com]
 
+https://github.com/joshlapham/instacli
+
 License: Beerware
 
-## Usage
+### What this does
 
-Be sure to add your Instapaper API key to the script.
+A simple Python script that takes a URL as an argument and adds to [Instapaper](https://instapaper.com).
 
-e.g. `python instacli.py http://thoughtmaybe.com` or `thoughtmaybe.com` will add the URL to Instapaper and return a message.
+### Usage
+
+Be sure to add your [Instapaper](https://instapaper.com) API key to the script.
+
+Run the script and supply a URL as an argument.
+
+e.g. `python instacli.py http://thoughtmaybe.com` or `thoughtmaybe.com` will add the URL to [Instapaper](https://instapaper.com) and return a message.
+
+### TODO
+
+* If status fails then prompt - [try again?, abort?].
+* Option to add Title and Description (blank is optional).
+* Check if strings are empty, omit from URL API data.
--- instacli.py
+++ instacli.py
@@ -3,9 +3,11 @@
 ###
 #
 # InstaCLI
-# Takes a URL as an argument and saves to Instapaper.
+# Takes a URL as an argument and adds to Instapaper.
 #
 # By Josh Lapham [josh@joshlapham.com]
+#
+# https://github.com/joshlapham/instacli
 #
 # License: Beerware
 #
@@ -14,23 +16,29 @@
 import sys
 import urllib
 
-# VARIABLES
+## VARIABLES
+# Instapaper username and password
 username = "YOUR_USER_NAME"
 password = "YOUR_PASSWORD"
 
-# FUNCTIONS
-# Post data to Instapaper API.
-def postToAPI():
-    # Put it all together into a URL to pass to Instapaper API.
+## FUNCTIONS
+# Post data to Instapaper API
+def postToAPI(urlstring):
+    # Put it all together into a URL to pass to Instapaper API
     urldata = "username=%s&password=%s&url=%s" % (username,password,urlstring)
     f = urllib.urlopen("https://www.instapaper.com/api/add?", urldata)
     s = f.read()
     f.close()
-    print s
+    if s == '201':
+    	print "URL successfully saved to Instapaper."
+    else:
+    	print "ERROR: Could not save URL to Instapaper."
     return
 
+## MAIN
+# Check if an argument was given; exit with usage message if none
 if len(sys.argv) == 1:
     sys.exit("Usage: insta [URL]")
+# If an argument was given then pass to postToAPI function
 elif len(sys.argv) >= 1:
-    urlstring = sys.argv[1]
-    postToAPI()
+    postToAPI(sys.argv[1])
